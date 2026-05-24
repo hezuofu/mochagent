ce79d8cd716a3f948d2e12ade255238d0c907f1e
hezuofu <hezuofu@mochaagents.dev>
2026-05-24 16:45:47 +0800

    Revert "refactor: SystemPromptProvider新增of()静态辅助"
    This reverts commit 9b214d92cfff39883d36d53cfb0e9d14937d6e42.

--- src/main/java/io/sketch/mochaagents/prompt/SystemPromptProvider.java
+++ src/main/java/io/sketch/mochaagents/prompt/SystemPromptProvider.java
@@ -4,12 +4,6 @@ package io.sketch.mochaagents.prompt;
  * Contract for agents that generate their own system prompt.
  * @author lanxia39@163.com
  */
-import io.sketch.mochaagents.agent.Agent;
-
 public interface SystemPromptProvider {
     String buildSystemPrompt();
-
-    static String of(Agent<?, ?> agent) {
-        return agent instanceof SystemPromptProvider spp ? spp.buildSystemPrompt() : "";
-    }
 }
